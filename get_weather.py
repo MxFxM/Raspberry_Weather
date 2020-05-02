7caa4f9d0d1105b49c8b075b8ec853659910fd21
MxFxM <mxfxmmueller@gmail.com>
2020-05-02 13:13:45 +0100

    InfluxDB added

--- get_weather.py
+++ get_weather.py
@@ -1,6 +1,7 @@
 import configparser
 import requests
 import sys
+from influxdb import InfluxDBClient
  
 def get_api_key():
     config = configparser.ConfigParser()
@@ -11,17 +12,36 @@ def get_weather(api_key, location):
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}".format(location, api_key)
     r = requests.get(url)
     return r.json()
+
+def get_body(weather):
+    json_body = [{"measurement": "Oberderdingen",
+                  "fields": {
+                      "Temperature": weather['main']['temp'],
+                      "Pressure": weather['main']['pressure'],
+                      "Humidity": weather['main']['humidity'],
+                      "Windspeed": weather['wind']['speed'],
+                      "Winddirection": weather['wind']['deg'],
+                      "Clouds": weather['clouds']['all']
+                      }
+                }]
+    return json_body
  
 def main():
+    location = ""
     if len(sys.argv) != 2:
-        exit("Usage: {} LOCATION".format(sys.argv[0]))
-    location = sys.argv[1]
+        #exit("Usage: {} LOCATION".format(sys.argv[0]))
+        location = "Oberderdingen" # use this if no location given
+    else:
+        location = sys.argv[1]
  
     api_key = get_api_key()
     weather = get_weather(api_key, location)
  
-    print(weather['main']['temp'])
+    #print(weather['main']['temp'])
     print(weather)
+
+    client = InfluxDBClient('localhost', 8086, 'root', 'root', 'WEATHER')
+    client.write_points(get_body(weather))
  
  
 if __name__ == '__main__':
